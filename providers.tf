510145b6e7a27779bc106531a05b5504098c71c8
gsowjanya97 <sowjanya.gade@outlook.com>
2023-06-15 13:13:08 +0530

    Provider 2nd update

--- providers.tf
+++ providers.tf
@@ -2,6 +2,7 @@ terraform {
     required_version = ">=0.12"
 
     required_providers {
+#Updated azurerm provider
         azurerm = {
             source = "hashicorp/azurerm"
             version = "3.61.0"
